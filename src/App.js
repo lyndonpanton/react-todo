a5fcb15f132b0acda8df9f9740bc03b7fe94f84a
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-01 18:05:51 +0100

    added array to state to hold todos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+import Input from "./Input";
 
 class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			todo: []
+		};
+	}
 	render() {
 		return (
 			<div className="app">
 				<h1>Hello React</h1>
+				<Input />
 			</div>
 		);
 	}
