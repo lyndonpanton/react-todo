d8a30982babe80157f96d3e9eee8aed75cf2ef62
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-01 18:08:39 +0100

    sent state todos to todo component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from "react";
 import Input from "./Input";
+import Todo from "./Todo";
 
 class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			todo: []
+			todos: [
+
+			]
 		};
 	}
+
 	render() {
 		return (
 			<div className="app">
 				<h1>Hello React</h1>
 				<Input />
+				<Todo todos={this.state.todos} />
 			</div>
 		);
 	}
