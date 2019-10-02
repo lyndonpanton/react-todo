f3429912c3fd54d2856e825515758c505e984ec6
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-02 17:16:06 +0100

    added click event listener to delete icons

--- src/Todo.js
+++ src/Todo.js
@@ -1,12 +1,16 @@
 import React from "react";
 
 class Todo extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
 	render() {
 		const todos = this.props.todos.map((key, index) => {
 			return (
 				<li key={index} className="todo">
 					<span className="todo-value">{ this.props.todos[index].value }</span>
-					<div className="cross">
+					<div className="cross" onClick={this.props.handleClick} >
 						<div className="cross-left"></div>
 						<div className="cross-right"></div>
 					</div>
