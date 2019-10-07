b967ecf41a03b47100d0e1b067336f1893e0a1ee
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-07 16:50:46 +0100

    changed font family, removed border radius

--- src/Button.js
+++ src/Button.js
@@ -2,9 +2,13 @@ import React from "react";
 
 const Button = (props) => {
 	const type = props.type;
+	const styles = {
+		borderRadius: "0",
+		fontFamily: "Palatino, Palatino Linotype, Palatino LT STD, Book Antiqua, Georgia, serif"
+	};
 
 	return (
-		<button onClick={(event) => props.clearChecked(event)}>{type}</button>
+		<button style={styles} onClick={(event) => props.clearChecked(event)}>{type}</button>
 	);
 }
 
