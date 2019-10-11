a0576222646ddf2fac3f1f91ddeb7c30f23d00ed
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-11 14:40:15 +0100

    removed useless constructor

--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,6 @@
 import React from "react";
 
 class Todo extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-
 	render() {
 
 		const todos = this.props.todos.map((key, index) => {
