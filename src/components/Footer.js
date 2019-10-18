6f928cfa8e259becc50ff71ba64fcd4af0711ee6
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-18 18:55:13 +0100

    populated component content with props

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 const Footer = (props) => {
+	const { author, copyright, date } = props;
+
 	return (
 		<footer>
-			<p></p>
+			<p>&copy; {date} {author} | {copyright}</p>
 		</footer>
 	);
 };
