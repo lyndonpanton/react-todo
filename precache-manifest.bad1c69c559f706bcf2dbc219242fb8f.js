620f86ae779636fb47f089bce338c02137773ff7
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-18 18:45:14 +0100

    Updates

---
+++ precache-manifest.bad1c69c559f706bcf2dbc219242fb8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a9f16ceef4fa02bc8417658d886b719",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "fa41f9d7b3468e2c0f08",
+    "url": "/react-todo/static/css/main.cc9bc746.chunk.css"
+  },
+  {
+    "revision": "7506cd78f5e7a07a0ed2",
+    "url": "/react-todo/static/js/2.78a4d2b6.chunk.js"
+  },
+  {
+    "revision": "fa41f9d7b3468e2c0f08",
+    "url": "/react-todo/static/js/main.0cb34aa8.chunk.js"
+  },
+  {
+    "revision": "d28ddac521141ae81880",
+    "url": "/react-todo/static/js/runtime-main.9dc3b792.js"
+  }
+]);
